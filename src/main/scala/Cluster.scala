7ba5e9d0a201b382d44fed1ca3b0e51bd489825e
Sagar Deshpande <deshpandesagar1704@gmail.com>
2016-04-08 21:24:48 -0500

    Added validation.

--- src/main/scala/Cluster.scala
+++ src/main/scala/Cluster.scala
@@ -13,24 +13,28 @@ object Cluster {
         val conf = new SparkConf().setAppName("movieCluster")
         val sc = new SparkContext(conf)
 
-        //        val itemUserMatrix = sc.textFile("S:\\Spring2016\\BigData\\Homeworks\\Homework3\\dataset\\itemusermat")
-        val itemUserMatrix = sc.textFile(args(0))
+        if (args.length != 2) {
+            println("Usage: [itemusermat] [movies]")
+        } else {
+            //        val itemUserMatrix = sc.textFile("S:\\Spring2016\\BigData\\Homeworks\\Homework3\\dataset\\itemusermat")
+            val itemUserMatrix = sc.textFile(args(0))
 
-        val data = itemUserMatrix.map(line => Vectors.dense(line.split(" ").drop(1).map(_.toDouble))).cache()
-        val kMeansModel = KMeans.train(data, k, maxIterations)
+            val data = itemUserMatrix.map(line => Vectors.dense(line.split(" ").drop(1).map(_.toDouble))).cache()
+            val kMeansModel = KMeans.train(data, k, maxIterations)
 
-        val movieRatings = itemUserMatrix.map(line => getMovieRatings(line)).map(item => (item._1, Vectors.dense(item._2.map(_.toDouble))))
+            val movieRatings = itemUserMatrix.map(line => getMovieRatings(line)).map(item => (item._1, Vectors.dense(item._2.map(_.toDouble))))
 
-        val clusters = movieRatings.map(item => (item._1, kMeansModel.predict(item._2)))
+            val clusters = movieRatings.map(item => (item._1, kMeansModel.predict(item._2)))
 
-        //        val moviesData = sc.textFile("S:\\Spring2016\\BigData\\Homeworks\\Homework3\\dataset\\movies.dat")
-        val moviesData = sc.textFile(args(1))
-        val movies = moviesData.map(line => line.split("::")).map(item => (item(0).toLong, item.mkString(",")))
+            //        val moviesData = sc.textFile("S:\\Spring2016\\BigData\\Homeworks\\Homework3\\dataset\\movies.dat")
+            val moviesData = sc.textFile(args(1))
+            val movies = moviesData.map(line => line.split("::")).map(item => (item(0).toLong, item.mkString(",")))
 
-        val moviesCluster = movies.join(clusters).map(item => item.swap).map(item => (item._1._2, item._1._1)).reduceByKey(_+"%"+_)
-        val output = moviesCluster.map(item => (item._1, item._2.split("%").take(5).mkString("\n\t"))).sortBy(_._1, true).map(item => ("Cluster" + item._1 + "\n\t" + item._2))
+            val moviesCluster = movies.join(clusters).map(item => item.swap).map(item => (item._1._2, item._1._1)).reduceByKey(_ + "%" + _)
+            val output = moviesCluster.map(item => (item._1, item._2.split("%").mkString("\n\t"))).sortBy(_._1, true).map(item => ("Cluster" + item._1 + "\n\t" + item._2))
 
-        output.saveAsTextFile(".\\output")
+            output.saveAsTextFile(".\\output")
+        }
     }
 
     def getMovieRatings(line: String): (Long, Array[String]) = {
